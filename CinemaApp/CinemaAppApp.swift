31b640e4c9dc5fe04622de5d7775ffa141a028c1
Miguel <mikisaraviaios@gmail.com>
2024-12-18 20:40:35 -0400

    Created project with swiftui

---
+++ CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Miguel Saravia on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
